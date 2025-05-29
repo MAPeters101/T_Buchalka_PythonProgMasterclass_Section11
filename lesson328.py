ed4d29500db4e21d7d1b6047e6b1973d30c2aad2
Mark Peters <mapeters101@earthlink.net>
2025-05-29 11:53:30 -0400

    Work on lesson328.

---
+++ lesson328.py
@@ -0,0 +1,17 @@
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
+
+print(tkinter.TkVersion)
+print(tkinter.TclVersion)
+
+#tkinter._test()
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Hello World")
+mainWindow.geometry('640x480+8+100')
+mainWindow.mainloop()
+
+
