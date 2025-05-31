c63c8c773f625d401bee28b303622cb44c98372d
Mark Peters <mapeters101@earthlink.net>
2025-05-30 20:34:20 -0400

    Work on lesson332 from 3:00 to 10:00.

--- lesson332.py
+++ lesson332.py
@@ -54,6 +54,20 @@ resultLabel.grid(row=2, column=2, sticky='nw')
 result = tkinter.Entry(mainWindow)
 result.grid(row=2, column=2, sticky='sw')
 
+# Frame for the time spinners
+timeFrame = tkinter.LabelFrame(mainWindow, text="Time")
+timeFrame.grid(row=3, column=0, sticky='new')
+# Time spinners
+hourSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 24)))
+minuteSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+secondSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+hourSpinner.grid(row=0, column=0)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=1)
+minuteSpinner.grid(row=0, column=2)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=3)
+secondSpinner.grid(row=0, column=4)
+timeFrame['padx'] = 36
+
 mainWindow.mainloop()
 
 print(rbValue.get())
